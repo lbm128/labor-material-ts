6bc44b466acabeb6ae497e6c8cd48849129f9dd3
Luciano <luciano.b.mogorovic@sherwin.com>
2023-08-23 09:56:36 -0400

    App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import Labor from './components/Labor';
+import Material from './components/Material';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/labor" element={<Labor />} />
+            <Route path="/material" element={<Material />} />
+          </Routes>
+        </Router>
+      </Provider>
     </div>
   );
 }
