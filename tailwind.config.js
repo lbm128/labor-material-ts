690ec54f2174b7eb921d5843f5f944e2f7e83a32
Luciano <luciano.b.mogorovic@sherwin.com>
2023-08-24 11:24:30 -0400

    install dc packages and add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('@prism/dropcloth/tailwind.preset')],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
