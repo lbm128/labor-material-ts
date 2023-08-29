002fced3278fae85521439c22c86c441d2b4f2bf
Luciano <luciano.b.mogorovic@sherwin.com>
2023-08-29 08:06:12 -0400

    formatting

--- src/mocks/handlers/laborhistory-handlers.ts
+++ src/mocks/handlers/laborhistory-handlers.ts
@@ -1,13 +1,13 @@
-import { rest } from "msw";
+import { rest } from 'msw';
 
 const apiBase = 'http://localhost:4000';
 
 const getLaborHistoryRespose = {
-    laborHistory: ['$123.00', '$456.00', '$5.00']
-}
+  laborHistory: ['$123.00', '$456.00', '$5.00'],
+};
 
 const laborHistoryHandlers = [
-    rest.get(`${apiBase}/laborHistory`, (_, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ ...getLaborHistoryRespose }));
-    }),
-  ];
+  rest.get(`${apiBase}/laborHistory`, (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ ...getLaborHistoryRespose }));
+  }),
+];
