24e905d28e5a19f246167cd864d1f00f16dfdc14
Luciano <luciano.b.mogorovic@sherwin.com>
2023-08-29 15:00:41 -0400

    new line

--- src/components/__tests__/Labor.test.tsx
+++ src/components/__tests__/Labor.test.tsx
@@ -3,6 +3,7 @@ import { act, screen, waitFor } from '@testing-library/react';
 import { renderWithProviders } from 'common/tests/utils';
 
 import Labor from 'components/Labor';
+
 describe('Labor', () => {
   test('should render properly', async () => {
     renderWithProviders(<Labor />);
