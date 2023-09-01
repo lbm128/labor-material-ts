01ca2b25645631cdd06b21f6dff04f6994699099
Luciano <luciano.b.mogorovic@sherwin.com>
2023-09-01 13:52:01 -0400

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client';
 import 'index.scss';
 import App from 'App';
 
+// if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  const { worker } = require('./mocks/browser');
+  worker.start();
+// // }
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
