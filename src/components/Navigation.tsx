5eba049d3072ffe5bd42c4e857c4cf3b696d503d
Luciano <luciano.b.mogorovic@sherwin.com>
2023-09-11 16:10:30 -0400

    found corners on nav menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,10 +13,8 @@ const Navigation = () => {
   return (
     <div className='navigation'>
       <div>
-        {/* <div className='swdc-button swdc-button--outlined swdc-mt-[30px]'> */}
-          <Button variant='outlined' className='swdc-button swdc-button--outlined swdc-mt-[30px]' onClick={handleShowMenu}>Menu</Button>
-        {/* </div> */}
-        <div className={`test-menu swdc-p-[15px] swdc-border-2 swdc-w-[40%] swdc-mt-[10px] swdc-mx-auto ${showMenu ? '' : 'swdc-hidden'}`}>
+        <Button variant='outlined' className='swdc-button swdc-button--outlined swdc-mt-[30px]' onClick={handleShowMenu}>Menu</Button>
+        <div className={`test-menu swdc-p-[15px] swdc-rounded-md swdc-border-2 swdc-w-[40%] swdc-mt-[10px] swdc-mx-auto ${showMenu ? '' : 'swdc-hidden'}`}>
           <div className='swdc-flex swdc-flex-col swdc-gap-[15px] '>
             <div className='nav-link-container'>
               <Link to='/' className='swdc-button swdc-button--filled' data-text='Home'>
