fe56e789203c36425bdf2dfc0771df9c76519812
Luciano <luciano.b.mogorovic@sherwin.com>
2023-09-12 10:51:09 -0400

    nav menu appears over content

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,7 +13,7 @@ const Navigation = () => {
     <div className='navigation'>
       <div>
         <Button variant='outlined' aria-label='menu' className='swdc-button swdc-button--outlined swdc-mt-[30px]' onClick={handleShowMenu}>Menu</Button>
-        <div title='navMenu' className={`test-menu swdc-p-[15px] swdc-rounded-md swdc-border-2 swdc-w-[40%] swdc-mt-[10px] swdc-mx-auto ${showMenu ? '' : 'swdc-hidden'}`}>
+        <div title='navMenu' className={`swdc-absolute swdc-inset-x-0 swdc-bg-white swdc-p-[15px] swdc-rounded-md swdc-border-2 swdc-w-[250px] swdc-mt-[10px] swdc-mx-auto ${showMenu ? '' : 'swdc-hidden'}`}>
           <div className='swdc-flex swdc-flex-col swdc-gap-[15px] '>
             <div className='nav-link-container'>
               <Link to='/' className='swdc-button swdc-button--filled' data-text='Home'>
